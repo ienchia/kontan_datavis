6dcf07a50ed717edf330fa207ef10a8f201fd28c
ienchia <ienchia@users.noreply.github.com>
2015-07-03 14:26:08 +0700

    Fix xhr bug to display accurate marker

--- javascript/maps.js
+++ javascript/maps.js
@@ -17,7 +17,7 @@ var icons = 'icon/star-3.png';
 
 $(document).ready(function () {
 
-    console.log("ready!");
+    console.log("document ready!");
     function initialize() {
 
         map = new google.maps.Map(document.getElementById('map-canvas'), {
@@ -58,139 +58,140 @@ $(document).ready(function () {
                 }
 
                 map.fitBounds(bounds);
-            });
-            // [END region_getplaces]
+        });
+        // [END region_getplaces]
 
-            // Bias the SearchBox results towards places that are within the bounds of the
-            // current map's viewport.
-            google.maps.event.addListener(map, 'bounds_changed', function () {
-                var bounds = map.getBounds();
-                searchBox.setBounds(bounds);
-            });
+        // Bias the SearchBox results towards places that are within the bounds of the
+        // current map's viewport.
+        google.maps.event.addListener(map, 'bounds_changed', function () {
+            var bounds = map.getBounds();
+            searchBox.setBounds(bounds);
+        });
 
-            // Add click event to place marker
-            google.maps.event.addListener(map, 'click', function (event) {
-                deleteMarker();
-                marker = new google.maps.Marker({
-                    map: map,
-                    position: event.latLng,
-                });
-
-                // Set circling area
-                circle = new google.maps.Circle({
-                    map: map,
-                    radius: slider.getValue() * 1000, // 10 miles in metres
-                    fillColor: '#AA0000'
-                });
-                circle.bindTo('center', marker, 'position');
-                markers.push(marker);
-                circles.push(circle);
-                // Set Zoom
-                if (map.zoom < 8) map.setZoom(8);
-                if (map.getBounds().ra.A < marker.getPosition().A || map.getBounds().za.A > marker.getPosition().A)
-                {
-                    map.setCenter(marker.getPosition());
-                }
-                latlng=event.latLng;
-                radius=slider.getValue()*1000;
-
-                getMarker(radius,latlng);
-            });
-        }
-
-        function changeRadius()
-        {
-            // Resize and Reposition Circle
-            circle.radius = slider.getValue() * 1000;
-            circle.bindTo('center', marker, 'position');
-
-            // Redraw Map
-            deletes();
-            getMarker(circle.radius,latlng);
-        }
-
-        function getMarker(radius, latlng){
-            var xmlhttp=new XMLHttpRequest();
-            xmlhttp.onreadystatechange=function() {
-                if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-
-                    var data=JSON.parse(xmlhttp.responseText);
-                    for(var i=0;i<data.length;i++){
-                        mapsProspek(data[i].lat,data[i].lng,data[i].description);
-                    }
-                }
-            };
-            xmlhttp.open("GET","class/latlong.php?lat="+latlng+"&rad="+radius,true);
-            xmlhttp.send();
-        };
-
-        function mapsProspek(lat, long, desc){
-
-            // Create infowindow
-            var infowindow = new google.maps.InfoWindow({
-                content: desc
-            });
-
-            var latlng=new google.maps.LatLng(lat,long);
-            var markerss=new google.maps.Marker({
+        // Add click event to place marker
+        google.maps.event.addListener(map, 'click', function (event) {
+            deleteMarker();
+            marker = new google.maps.Marker({
                 map: map,
-                position: latlng,
-                icon: icons,
-                title: desc
+                position: event.latLng,
             });
 
-            google.maps.event.addListener(markerss, 'click', function() {
-
-                // Only close if not null
-                closeCurrentInfoWindow();
-
-                infowindow.open(map,markerss);
-                currentInfoWindow = infowindow;
+            // Set circling area
+            circle = new google.maps.Circle({
+                map: map,
+                radius: slider.getValue() * 1000, // 10 miles in metres
+                fillColor: '#AA0000'
             });
-            mark.push(markerss);
-        }
+            circle.bindTo('center', marker, 'position');
+            
+            markers.push(marker);
+            circles.push(circle);
+            // Set Zoom
+            if (map.zoom < 8) map.setZoom(8);
+            if (map.getBounds().ra.A < marker.getPosition().A || map.getBounds().za.A > marker.getPosition().A)
+            {
+                map.setCenter(marker.getPosition());
+            }
+            latlng=event.latLng;
+            radius=slider.getValue()*1000;
 
-        function closeCurrentInfoWindow(){
-            // Close Active Current Info Window
-            if(currentInfoWindow!=null){
-                currentInfoWindow.close();
-            }
-        }
+            getMarker(radius,latlng);
+        });
+    }
 
-        function deletes(){
-            if (mark) {
-                for (i in mark) {
-                    mark[i].setMap(null);
-                }
-                mark.length = 0;
-            }
-        }
+    function changeRadius()
+    {
+        // Resize and Reposition Circle
+        circle.radius = slider.getValue() * 1000;
+        circle.bindTo('center', marker, 'position');
 
-        function deleteMarker() {
-            if (markers) {
-                for (i in markers) {
-                    markers[i].setMap(null);
-                }
-                markers.length = 0;
-            }
-            if (mark) {
-                for (i in mark) {
-                    mark[i].setMap(null);
-                }
-                mark.length = 0;
-            }
-            if (circles) {
-                for (i in circles) {
-                    circles[i].setMap(null);
-                }
-                circles.length = 0;
-            }
-        }
+        // Redraw Map
+        getMarker(circle.radius,latlng);
+    }
 
-        google.maps.event.addDomListener(window, 'load', initialize);
-        slider = new Slider('#radius', {
-            formatter: function (value) {
-                return value + ' Km';
+    function getMarker(radius, latlng){
+        var xmlhttp=new XMLHttpRequest();
+        xmlhttp.onreadystatechange=function() {
+            if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+
+                deletes();
+                var data=JSON.parse(xmlhttp.responseText);
+                for(var i=0;i<data.length;i++){
+                    mapsProspek(data[i].lat,data[i].lng,data[i].description);
+                }
             }
-        }).on('change', changeRadius);
-    });
+        };
+        xmlhttp.open("GET","class/latlong.php?lat="+latlng+"&rad="+radius,true);
+        xmlhttp.send();
+    };
+
+    function mapsProspek(lat, long, desc){
+
+        // Create infowindow
+        var infowindow = new google.maps.InfoWindow({
+            content: desc
+        });
+
+        var latlng=new google.maps.LatLng(lat,long);
+        var markerss=new google.maps.Marker({
+            map: map,
+            position: latlng,
+            icon: icons,
+            title: desc
+        });
+
+        google.maps.event.addListener(markerss, 'click', function() {
+
+            // Only close if not null
+            closeCurrentInfoWindow();
+
+            infowindow.open(map,markerss);
+            currentInfoWindow = infowindow;
+        });
+        mark.push(markerss);
+    }
+
+    function closeCurrentInfoWindow(){
+        // Close Active Current Info Window
+        if(currentInfoWindow!=null){
+            currentInfoWindow.close();
+        }
+    }
+
+    function deletes(){
+        if (mark) {
+            for (i in mark) {
+                mark[i].setMap(null);
+            }
+            mark.length = 0;
+        }
+    }
+
+    function deleteMarker() {
+        if (markers) {
+            for (i in markers) {
+                markers[i].setMap(null);
+            }
+            markers.length = 0;
+        }
+        if (mark) {
+            for (i in mark) {
+                mark[i].setMap(null);
+            }
+            mark.length = 0;
+        }
+        if (circles) {
+            for (i in circles) {
+                circles[i].setMap(null);
+            }
+            circles.length = 0;
+        }
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+    slider = new Slider('#radius', {
+        formatter: function (value) {
+            return value + ' Km';
+        }
+    }).on('change', changeRadius);
+});
